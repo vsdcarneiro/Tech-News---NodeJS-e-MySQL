cef87e71d8be2b608e99c85fe732e75f852cfb86
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-29 18:32:14 -0300

    create database connection

---
+++ database/connection.js
@@ -0,0 +1,16 @@
+import config from "config";
+import { Sequelize } from "sequelize";
+
+const { uri } = config.get("mysql");
+const sequelize = new Sequelize(uri);
+
+async function mysqlConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+export { sequelize, mysqlConnection };
