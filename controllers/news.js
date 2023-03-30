fe8c350f33530e602d9b7b6b5815322d0a4018ee
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-30 19:35:20 -0300

    create methods

--- controllers/news.js
+++ controllers/news.js
@@ -1,6 +1,30 @@
+import operationsCategory from "../database/categories/queries.js";
+import operationsNews from "../database/news/queries.js";
+
 const newsController = {
-  news: async (req, res) => {
-    res.send("news index route");
+  newNews: async (req, res) => {
+    try {
+      const result = await operationsCategory.getCategories();
+
+      if (result) {
+        res.render("admin/news/new", { categories: result });
+      }
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  },
+
+  createNews: async (req, res) => {
+    try {
+      const { title } = req.body;
+      const { description } = req.body;
+      const categoryId = parseInt(req.body.category);
+      
+      await operationsNews.createNews(title, description, categoryId);
+      res.redirect("/admin/news/new");
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
   },
 };
 
