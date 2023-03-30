544b631a0a62ade1918c2747c982f154fbd693a9
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-30 19:32:59 -0300

    change routes

--- routes/categories.js
+++ routes/categories.js
@@ -4,13 +4,10 @@ import categoriesController from "../controllers/categories.js";
 const router = Router();
 
 router.get("/admin/categories/new", categoriesController.newCategory);
-router.post("/admin/categories/create", categoriesController.createCategory);
+router.post("/categories/create", categoriesController.createCategory);
 router.get("/admin/categories", categoriesController.getCategories);
 router.get("/admin/categories/edit/:id", categoriesController.editCategory);
-router.post(
-  "/admin/categories/update",
-  categoriesController.updateCategory
-);
-router.post("/admin/categories/delete", categoriesController.deleteCategory);
+router.post("/categories/update", categoriesController.updateCategory);
+router.post("/categories/delete", categoriesController.deleteCategory);
 
 export default router;
