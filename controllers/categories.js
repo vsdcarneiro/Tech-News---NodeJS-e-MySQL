18c4b90098967bbcbb3f6dd94f6d9f1909123b98
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-30 03:16:30 -0300

    adjust and create methods

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,20 +1,83 @@
-import { createCategory } from "../database/categories/queries.js";
+import operations from "../database/categories/queries.js";
 
 const categoriesController = {
   newCategory: async (req, res) => {
-    res.render("admin/categories/new.ejs");
+    res.render("admin/categories/new");
   },
 
-  saveCategory: async (req, res) => {
+  createCategory: async (req, res) => {
     try {
-      const category = req.body.category;
+      const { category } = req.body;
 
       if (category) {
-        const result = await createCategory(category);
-        res.status(201).json(result);
+        await operations.createCategory(category);
+        res.redirect("/admin/categories");
       } else {
         res.redirect("/admin/categories/new");
       }
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  },
+
+  getCategories: async (req, res) => {
+    try {
+      const result = await operations.getCategories();
+
+      if (result) {
+        res.render("admin/categories/index", { categories: result });
+      } else {
+        res.redirect("admin/categories");
+      }
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  },
+
+  editCategory: async (req, res) => {
+    try {
+      let id;
+      let result;
+
+      if (!isNaN(req.params.id)) {
+        id = parseInt(req.params.id);
+        result = await operations.getCategoryById(id);
+
+        if (result) {
+          res.render("admin/categories/edit", { category: result });
+        } else {
+          res.redirect("/admin/categories");
+        }
+      } else {
+        return res.redirect("/admin/categories");
+      }
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  },
+
+  updateCategory: async (req, res) => {
+    try {
+      const [id, category] = [parseInt(req.body.id), req.body.category];
+
+      if (category) {
+        await operations.updateCategory(id, category);
+        res.redirect("/admin/categories");
+      } else {
+        res.render("admin/categories/edit", {
+          category: { id, category },
+        });
+      }
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  },
+
+  deleteCategory: async (req, res) => {
+    try {
+      const id = parseInt(req.body.id);
+      await operations.deleteCategory(id);
+      res.redirect("/admin/categories");
     } catch (error) {
       res.status(500).send("Internal Server Error");
     }
