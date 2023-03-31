1ba5f998e40701f6ffc8ab00509dbe2fdb03f4c9
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-30 23:02:49 -0300

    create database operations

--- database/news/queries.js
+++ database/news/queries.js
@@ -1,5 +1,6 @@
 import slugify from "slugify";
 import News from "../../models/News.js";
+import Category from "../../models/Category.js";
 
 const operationsNews = {
   createNews: async (title, description, categoryId) => {
@@ -14,6 +15,48 @@ const operationsNews = {
       if (result instanceof News) {
         return true;
       }
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getNews: async () => {
+    try {
+      const result = await News.findAll({ include: [{ model: Category }] });
+
+      if (result.every((news) => news instanceof News)) {
+        return result;
+      }
+      return undefined;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getNewsById: async (id) => {
+    try {
+      const result = await News.findByPk(id);
+
+      if (result instanceof News) {
+        return result;
+      }
+      return undefined;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateNews: async (id, news) => {
+    try {
+      await News.update({ news }, { where: { id } });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteNews: async (id) => {
+    try {
+      await News.destroy({ where: { id } });
     } catch (error) {
       throw error;
     }
