ac936335db6d8073c543eec0d1b4ab522f6703c9
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-30 23:02:13 -0300

    create methods

--- controllers/news.js
+++ controllers/news.js
@@ -19,9 +19,44 @@ const newsController = {
       const { title } = req.body;
       const { description } = req.body;
       const categoryId = parseInt(req.body.category);
-      
+
       await operationsNews.createNews(title, description, categoryId);
-      res.redirect("/admin/news/new");
+      res.redirect("/admin/news");
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  },
+
+  getNews: async (req, res) => {
+    try {
+      const result = await operationsNews.getNews();
+
+      if (result) {
+        res.render("admin/news/index", { news: result });
+      } else {
+        res.redirect("/admin/news");
+      }
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  },
+
+  editNews: async (req, res) => {
+    try {
+    } catch (error) {}
+  },
+
+  updateNews: async (req, res) => {
+    try {
+    } catch (error) {}
+  },
+
+  deleteNews: async (req, res) => {
+    try {
+      const id = parseInt(req.body.id);
+
+      await operationsNews.deleteNews(id);
+      res.redirect("/admin/news");
     } catch (error) {
       res.status(500).send("Internal Server Error");
     }
