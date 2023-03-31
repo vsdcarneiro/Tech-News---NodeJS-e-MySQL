929112d393a4b53dcc913542907a3cf444166947
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-31 02:37:37 -0300

    create route

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import News from '../models/News.js'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const result = await News.findAll()
+
+  if (result.every((news) => news instanceof News)) {
+    res.render('index', { news: result })
+  }
+})
+
+export default router
