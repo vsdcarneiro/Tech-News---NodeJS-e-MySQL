02c34010d72e05a92dbc588834c3e332d481f781
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-04-03 18:40:01 -0300

    create routes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ import indexController from '../controllers/index.js';
 const router = Router();
 
 router.get('/', indexController.getNews);
+router.get('/page/:num', indexController.getNewsByPage);
 router.get('/:slug', indexController.getNewsBySlug);
 router.get('/categories/:slug', indexController.getNewsByCategory);
-router.get('/page/:num', indexController.getNewsByPage);
+router.get('/categories/:slug/page/:num', indexController.getNewsByCategory);
 
 export default router;
