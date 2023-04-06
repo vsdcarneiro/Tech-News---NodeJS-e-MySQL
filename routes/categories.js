e1d939f4fee0ca4d3c9011cc3eb28d178187ad7c
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-04-06 00:13:43 -0300

    use middleware

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,25 @@
 import { Router } from 'express';
 import categoriesController from '../controllers/categories.js';
+import { adminAuthenticate } from '../middlewares/adminAuth.js';
 
 const router = Router();
 
-router.get('/admin/categories/new', categoriesController.newCategory);
+router.get(
+  '/admin/categories/new',
+  adminAuthenticate,
+  categoriesController.newCategory,
+);
 router.post('/categories/create', categoriesController.createCategory);
-router.get('/admin/categories', categoriesController.getCategories);
-router.get('/admin/categories/edit/:id', categoriesController.editCategory);
+router.get(
+  '/admin/categories',
+  adminAuthenticate,
+  categoriesController.getCategories,
+);
+router.get(
+  '/admin/categories/edit/:id',
+  adminAuthenticate,
+  categoriesController.editCategory,
+);
 router.post('/categories/update', categoriesController.updateCategory);
 router.post('/categories/delete', categoriesController.deleteCategory);
 
--- routes/news.js
+++ routes/news.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import newsController from '../controllers/news.js';
+import { adminAuthenticate } from '../middlewares/adminAuth.js';
 
 const router = Router();
 
-router.get('/admin/news/new', newsController.newNews);
+router.get('/admin/news/new', adminAuthenticate, newsController.newNews);
 router.post('/news/create', newsController.createNews);
-router.get('/admin/news', newsController.getNews);
-router.get('/admin/news/edit/:id', newsController.editNews);
+router.get('/admin/news', adminAuthenticate, newsController.getNews);
+router.get('/admin/news/edit/:id', adminAuthenticate, newsController.editNews);
 router.post('/news/update', newsController.updateNews);
 router.post('/news/delete', newsController.deleteNews);
 
