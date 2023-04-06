28580943fe151beac64ce8169e651d1bdadacbc9
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-04-06 00:12:42 -0300

    create routes

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import usersController from '../controllers/users.js';
+import { adminAuthenticate } from '../middlewares/adminAuth.js';
+
+const router = Router();
+
+router.get('/admin/users/new', adminAuthenticate, usersController.newUser);
+router.post('/users/create', usersController.createUser);
+router.get('/admin/users', adminAuthenticate, usersController.getUsers);
+router.get('/users/login', usersController.loginUser);
+router.post('/users/authenticate', usersController.authenticateUser);
+router.get('/users/logout', usersController.logoutUser);
+
+export default router;
